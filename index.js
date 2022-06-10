86c2e456a2e9bddaaaff33375508ab5e6e265e01
Steven Taylor <ninjasteve00@hotmail.com>
2022-06-09 20:02:52 -0400

    added elements

--- index.js
+++ index.js
@@ -19,4 +19,44 @@ let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
 
+let descriptionHeader= document.createElement('h3')
+descriptionHeader.append('Description:')
+
+let descriptionParagraph= document.createElement('p')
+descriptionParagraph.append(`
+    This gentle dog is aloof toward her owner, and never comes when called.
+    She always acts as though any stranger she meets will harm her,
+    and dislikes other animals.
+`)
+
+let feedingTimesHeader = document.createElement('h3')
+feedingTimesHeader.append('Feeding Times:')
+
+let feedingTimeList = document.createElement('ul')
+
+let feedingTime1 = document.createElement('li')
+feedingTime1.append('9:00 am')
+
+let feedingTime2 = document.createElement('li')
+feedingTime2.append('12:00 pm')
+
+let feedingTime3 = document.createElement('li')
+feedingTime3.append('5:00 pm')
+
+feedingTimeList.append(
+    feedingTime1,
+    feedingTime2,
+    feedingTime3,
+)
+
+dogDetails.append(
+    descriptionHeader,
+    descriptionParagraph,
+    feedingTimesHeader,
+    feedingTimeList
+)
+
+dogContent.append(dogImage, dogDetails)
+
+content.append(header, dogContent)
 
